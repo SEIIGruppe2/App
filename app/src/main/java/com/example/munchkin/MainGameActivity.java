ce26cf83fe0aef2a312b904080ec4523604c7f91
Yvonne Egger <y1egger@edu.aau.at>
2024-04-07 15:39:35 +0200

    feat: Update MainGameActivity.java, MainGameView.java and AndroidManifest.xml
    Updated two Java classes to implement new features or improvements. Also, made necessary changes to the AndroidManifest.xml file to reflect updates in the application configuration.
    
    Files modified:
    - Modified MainGameActivity.java
    - Modified MainGameView.java
    - Modified AndroidManifest.xml

--- app/src/main/java/com/example/munchkin/MainGameActivity.java
+++ app/src/main/java/com/example/munchkin/MainGameActivity.java
@@ -1,4 +1,25 @@
 package com.example.munchkin;
 
-public class MainGameActivity {
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.munchkin.view.ConnectToServerView;
+import com.example.munchkin.view.MainGameView;
+
+public class MainGameActivity extends AppCompatActivity {
+
+    private MainGameView mainGameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_game);
+
+        mainGameView = new MainGameView(this);
+    }
 }
