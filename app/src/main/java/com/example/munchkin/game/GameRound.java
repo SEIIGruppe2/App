488acd48af461d8feaf200ec3ffde997f3ad362f
Manuel1598 <manuel.weilguni1@gmail.com>
2024-04-15 18:01:07 +0200

    feat: Hinzufügen von Interfaces für die Kommunikation
    - Interfaces für die Klasse GameRound, damit diese unabhängig von den anderen Klassen arbeiten kann

--- app/src/main/java/com/example/munchkin/game/GameRound.java
+++ app/src/main/java/com/example/munchkin/game/GameRound.java
@@ -1,29 +1,36 @@
 package com.example.munchkin.game;
 
 import com.example.munchkin.Player.Player;
+import com.example.munchkin.interfaces.GameEventHandler;
+import com.example.munchkin.model.DiceRollModel;
+
+
 
 public class GameRound {
 
     private Player currentPlayer;
     private int[] diceResults;
+    private GameEventHandler gameEventHandler;
 
-    public GameRound(Player currentPlayer) {
+    public GameRound(Player currentPlayer, GameEventHandler gameEventHandler) {
         this.currentPlayer = currentPlayer;
+        this.gameEventHandler = gameEventHandler;
         diceResults = new int[3];
     }
 
     public void start() {
-        rollDice();
+        gameEventHandler.requestDiceRoll(this::handleDiceResults);
+    }
+
+
+    private void handleDiceResults(int[] results) {
+        this.diceResults = results;
         placeMonsters();
-
+        // Weiter Logik nach dem Würfeln
     }
 
-    private void rollDice() {
-
-    }
 
     private void placeMonsters() {
-
 
 
     }
