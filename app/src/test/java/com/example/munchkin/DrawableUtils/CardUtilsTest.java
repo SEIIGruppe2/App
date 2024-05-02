b4e0c38ecccf5f5b7e2c13b87dcb4f7d7c249f6c
Renato Pliso <renatopl@edu.aau.at>
2024-05-02 15:24:59 +0200

    refactor: ChangeD access modifiers to protected for CardUtilsTest
    Updated access modifiers to protected for better encapsulation and inheritance. This change enhances the readability and maintainability of the codebase.
    
    Files modified:
    - Updated CardUtilsTest

--- app/src/test/java/com/example/munchkin/DrawableUtils/CardUtilsTest.java
+++ app/src/test/java/com/example/munchkin/DrawableUtils/CardUtilsTest.java
@@ -10,42 +10,42 @@ import java.util.Collections;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CardUtilsTest {
+class CardUtilsTest {
     @Test
-    public void testGetResources() {
+    protected void testGetResources() {
         List<ActionCardDTO> cards = Arrays.asList(
                 new ActionCardDTO("Bogenschütze", 0, 1),
                 new ActionCardDTO("Schwertkämpfer", 1, 2),
                 new ActionCardDTO("Held", 2, 3),
                 new ActionCardDTO("Ritter", 3, 4),
-                new ActionCardDTO("Unknown", 4, 5) // Testing default cases
+                new ActionCardDTO("Unknown", 4, 5)
         );
         String[] expected = {"blauerbogenschuetze", "roterschwertkaempfer", "gruenerheld", "braunerritter", "blauerheld"};
         assertArrayEquals(expected, CardUtils.getresources(cards));
     }
 
     @Test
-    public void testGetResourcesWithEmptyList() {
+    protected void testGetResourcesWithEmptyList() {
         assertTrue(CardUtils.getresources(Collections.emptyList()).length == 0);
     }
 
     @Test
-    public void testGetCharacter() {
+    protected void testGetCharacter() {
         assertEquals("bogenschuetze", CardUtils.getcharacter("Bogenschütze"));
         assertEquals("schwertkaempfer", CardUtils.getcharacter("Schwertkämpfer"));
         assertEquals("held", CardUtils.getcharacter("Held"));
         assertEquals("ritter", CardUtils.getcharacter("Ritter"));
-        // Test default case
+
         assertEquals("held", CardUtils.getcharacter("Unknown"));
     }
 
     @Test
-    public void testGetZone() {
+    protected void testGetZone() {
         assertEquals("blauer", CardUtils.getzone(0));
         assertEquals("roter", CardUtils.getzone(1));
         assertEquals("gruener", CardUtils.getzone(2));
         assertEquals("brauner", CardUtils.getzone(3));
-        // Test default case
+
         assertEquals("blauer", CardUtils.getzone(4));
     }
 
