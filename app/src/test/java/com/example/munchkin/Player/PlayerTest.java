d65db56d0342a26c6c8683848f662700497627ba
Yvonne Egger <y1egger@edu.aau.at>
2024-05-05 09:57:11 +0200

    feat: modified PlayerTest.java
    Added functionality.
    
    Files modified:
    - modified PlayerTest.java

--- app/src/test/java/com/example/munchkin/Player/PlayerTest.java
+++ app/src/test/java/com/example/munchkin/Player/PlayerTest.java
@@ -1,7 +1,30 @@
 package com.example.munchkin.Player;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class PlayerTest {
+
+    @Test
+    protected void testDefaultConstructor() {
+
+        Player player = new Player();
+        assertNull(player.getName(), "Name should be null for default constructor");
+
+    }
+
+    @Test
+    protected void testParameterizedConstructor() {
+
+        String testName = "Username";
+        Player player = new Player(testName);
+        assertEquals(testName, player.getName(), "Name should match the one set in the constructor");
+
+    }
+
+
+
 
 }
