47536c3723be8eee611726844334661eedaa6bf7
Yvonne Egger <y1egger@edu.aau.at>
2024-05-05 10:04:04 +0200

    feat: modified GameRoundTest.java
    Added functionality.
    
    Files modified:
    - modified GameRoundTest.java

--- app/src/test/java/com/example/munchkin/game/GameRoundTest.java
+++ app/src/test/java/com/example/munchkin/game/GameRoundTest.java
@@ -1,7 +1,39 @@
 package com.example.munchkin.game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.munchkin.Player.Player;
+import com.example.munchkin.interfaces.GameEventHandler;
+import org.junit.jupiter.api.BeforeEach;
 
-class GameRoundTest {
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
+public class GameRoundTest {
+
+    @Mock
+    private Player mockPlayer;
+    @Mock
+    private GameEventHandler mockEventHandler;
+
+    private GameRound gameRound;
+
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        gameRound = new GameRound(mockPlayer, mockEventHandler);
+    }
+
+    @Test
+    public void testConstructor() {
+
+    }
+
+
+
+
 
 }
+
