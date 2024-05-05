df27c991e2e5dce3c11e7c1b307d6d5e0774833d
Yvonne Egger <y1egger@edu.aau.at>
2024-05-05 10:20:23 +0200

    refactor: Clean up PlayerTest.java
    Performed cleanup and refactoring in the test class to improve readability, organization, and maintainability.
    
    Files modified:
    - Updated PlayerTest.java

--- app/src/test/java/com/example/munchkin/Player/PlayerTest.java
+++ app/src/test/java/com/example/munchkin/Player/PlayerTest.java
@@ -1,7 +1,6 @@
 package com.example.munchkin.Player;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -9,22 +8,14 @@ class PlayerTest {
 
     @Test
     protected void testDefaultConstructor() {
-
         Player player = new Player();
         assertNull(player.getName(), "Name should be null for default constructor");
-
     }
 
     @Test
     protected void testParameterizedConstructor() {
-
         String testName = "Username";
         Player player = new Player(testName);
         assertEquals(testName, player.getName(), "Name should match the one set in the constructor");
-
     }
-
-
-
-
 }
