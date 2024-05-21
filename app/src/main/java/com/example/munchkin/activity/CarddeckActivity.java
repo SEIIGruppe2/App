751f69b1a35735e4b3cb7b242490f6ce3794a665
Manuel1598 <manuel.weilguni1@gmail.com>
2024-05-21 13:11:54 +0200

    refactor: remove unused import

--- app/src/main/java/com/example/munchkin/activity/CarddeckActivity.java
+++ app/src/main/java/com/example/munchkin/activity/CarddeckActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
