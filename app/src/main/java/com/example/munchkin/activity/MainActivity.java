7241e57ccc437e78e9e4f005e01e2b385bf350a9
Manuel1598 <manuel.weilguni1@gmail.com>
2024-05-22 10:15:31 +0200

    refactor: deleted unused import and field

--- app/src/main/java/com/example/munchkin/activity/MainActivity.java
+++ app/src/main/java/com/example/munchkin/activity/MainActivity.java
@@ -3,22 +3,15 @@ package com.example.munchkin.activity;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.munchkin.R;
-import com.example.munchkin.view.MainView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MainView mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-
-        mainView = new MainView(this);
     }
 
 }
