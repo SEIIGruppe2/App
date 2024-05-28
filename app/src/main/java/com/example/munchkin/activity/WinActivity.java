0c745328bf224973dc069f0fbce3c791726212b5
Manuel1598 <manuel.weilguni1@gmail.com>
2024-05-28 11:30:48 +0200

    feat: Customize the Win activity and the Win screen
    -so that the winner's name can be displayed

--- app/src/main/java/com/example/munchkin/activity/WinActivity.java
+++ app/src/main/java/com/example/munchkin/activity/WinActivity.java
@@ -3,6 +3,8 @@ package com.example.munchkin.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.munchkin.R;
 
@@ -11,6 +13,12 @@ public class WinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.win_screen);
+
+        String winnerName = getIntent().getStringExtra("winnerName");
+
+
+        TextView textViewWin = findViewById(R.id.textViewWin);
+        textViewWin.setText("Herzlichen Glückwunsch " + winnerName + "! Du hast gewonnen!");
 
         Button homeButton = findViewById(R.id.homeButtonWin);
         homeButton.setOnClickListener(v -> {
