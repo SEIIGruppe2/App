172306ea0d096fd433679226cafed6241b48bab0
TimonWoelbitsch <100867226+TimonWoelbitsch@users.noreply.github.com>
2024-06-07 20:34:58 +0200

    added button for gamerules

--- app/src/main/java/com/example/munchkin/activity/GameRules.java
+++ app/src/main/java/com/example/munchkin/activity/GameRules.java
@@ -1,6 +1,8 @@
 package com.example.munchkin.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +18,13 @@ public class GameRules extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.fragment_game_rules);
+        setContentView(R.layout.game_rules);
+        Button goBack = findViewById(R.id.goBack);
+        goBack.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
