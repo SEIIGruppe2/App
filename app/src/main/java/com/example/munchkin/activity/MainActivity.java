bf2226987faa73e861d4bc1f353dae623ad615b6
Manuel1598 <manuel.weilguni1@gmail.com>
2024-06-16 14:36:40 +0200

    refactor: removed unused imports and removed commented out code

--- app/src/main/java/com/example/munchkin/activity/MainActivity.java
+++ app/src/main/java/com/example/munchkin/activity/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.munchkin.activity;
 
-import android.app.FragmentManager;
-import android.media.AudioAttributes;
+
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.example.munchkin.Options;
 import com.example.munchkin.R;
 import com.example.munchkin.view.MainView;
 
@@ -23,29 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-
-        // Load the initial view
-
         setContentView(R.layout.activity_main); mainView = new MainView(this);
-
-
-        // Example of switching views on some action, like button click
         findViewById(R.id.buttonOptions).setOnClickListener(view -> {
 
         });
-
-        /*mediaPlayer1 = MediaPlayer.create(this, R.raw.munchkinpanic);
-
-        AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_MEDIA)
-                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                .setLegacyStreamType(AudioAttributes.USAGE_MEDIA)
-                .build();
-        mediaPlayer1.setAudioAttributes(audioAttributes);*/
-
-
-
-
     }
 
     public void startOptions(){
@@ -53,13 +31,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startmusic(){
-
         // Initialize the MediaPlayer with the correct audio attributes
-
         mediaPlayer1.start();
-
-
     }
+
+
 
     private void loadView(Fragment fragment) {
         getSupportFragmentManager()
