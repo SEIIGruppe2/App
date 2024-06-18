eaa91baea4a54b25223b7bce090681a494da7b95
Renato Pliso <renatopl@edu.aau.at>
2024-06-18 22:56:19 +0200

    ref: Changed textViewArray from public to protected
    Changes made in LobbyView

--- app/src/main/java/com/example/munchkin/view/LobbyView.java
+++ app/src/main/java/com/example/munchkin/view/LobbyView.java
@@ -14,7 +14,7 @@ import com.example.munchkin.controller.LobbyController;
 public class LobbyView {
 
     LobbyActivity lobbyActivity;
-    public static final TextView[] textViewArray = new TextView[4];
+    protected static final TextView[] textViewArray = new TextView[4];
     public LobbyView( LobbyActivity lobbyActivity){
         this.lobbyActivity= lobbyActivity;
 
